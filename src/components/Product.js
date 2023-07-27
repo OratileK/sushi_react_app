6e5f6053935abd7efd0f016f23c8b11d2bc4ae6d
OratileK <111352164+OratileK@users.noreply.github.com>
2023-07-27 22:42:05 +0200

    Update Product.js

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,18 +1,1 @@
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import { CartContext } from './CartContext';
 
-const Product = ({ product }) => {
-  const { addToCart } = useContext(CartContext);
-
-  return (
-    <div>
-      <h3>{product.name}</h3>
-      <p>Price: ${product.price}</p>
-      <button onClick={() => addToCart(product)}>Add to Cart</button>
-      <Link to="/cart">View Cart</Link>
-    </div>
-  );
-};
-
-export default Product;
